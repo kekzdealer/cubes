813be7da973a3e1629518c87513cb24c8c3b5e02
kekzdealer <kekzdealer@gmail.com>
2021-09-26 20:33:14 +0200

    added default shader program implementation

---
+++ src/main/java/at/kurumi/graphics/DefaultShader.java
@@ -0,0 +1,45 @@
+package at.kurumi.graphics;
+
+import at.kurumi.util.GraphicsException;
+import org.joml.Matrix4fc;
+
+import java.io.IOException;
+
+public class DefaultShader extends ShaderProgram {
+
+    private static final String NAME = "default";
+
+    // Textures
+    private int location_diffuseMap;
+    private int location_transformation;
+    private int location_projection;
+
+    public DefaultShader() throws GraphicsException, IOException {
+        super(NAME, NAME);
+    }
+
+    @Override
+    protected void bindAttributes() {
+        super.bindAttribute(0, "position");
+        super.bindAttribute(1, "texCoord");
+    }
+
+    @Override
+    protected void getAllUniformLocations() {
+        location_diffuseMap = super.getUniformLocation("diffuse");
+        location_transformation = super.getUniformLocation("transformation");
+        location_projection = super.getUniformLocation("projection");
+    }
+
+    public void uploadTexture(int textureBank) {
+        super.loadInt(location_diffuseMap, textureBank);
+    }
+
+    public void uploadTransformation(Matrix4fc transformation) {
+        super.loadMatrix4f(location_transformation, transformation);
+    }
+
+    public void uploadProjection(Matrix4fc projection) {
+        super.loadMatrix4f(location_projection, projection);
+    }
+}
