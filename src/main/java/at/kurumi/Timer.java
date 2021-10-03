1fe21fede3b833bf9fdfb018d1c1670b66c87e35
kekzdealer <kekzdealer@gmail.com>
2021-10-03 11:38:00 +0200

    added Timer class for time stuff

---
+++ src/main/java/at/kurumi/Timer.java
@@ -0,0 +1,25 @@
+package at.kurumi;
+
+public class Timer {
+
+    private double lastLoopTime;
+
+    public void init() {
+        lastLoopTime = getTime();
+    }
+
+    public double getTime() {
+        return System.nanoTime() / 1_000_000_000.0;
+    }
+
+    public double getElapsedTime() {
+        final var now = getTime();
+        final var elapsedTime = now - lastLoopTime;
+        lastLoopTime = now;
+        return elapsedTime;
+    }
+
+    public double getLastLoopTime() {
+        return lastLoopTime;
+    }
+}
